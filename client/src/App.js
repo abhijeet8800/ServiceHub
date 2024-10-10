2e817b6abcb0de4fb29c285ba29d55323e3ddc0b
abhijeet8800 <99193013+abhijeet8800@users.noreply.github.com>
2024-10-10 23:09:16 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,8 +13,7 @@ import {useNavigate} from "react-router-dom";
 import {Toaster} from "react-hot-toast";
 import BookingComplete from "./components/bookingcomplete";
 const AuthContext=React.createContext();
-// export const backendUrl="https://servicehubapp.onrender.com";
-export const backendUrl="http://localhost:5000";
+export const backendUrl="https://servicehub-tmwy.onrender.com";
 function App() {
   return (
     <Router>
